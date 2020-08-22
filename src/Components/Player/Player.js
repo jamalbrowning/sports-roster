0947a296c9b40126fb88231ba8d65e44f3c53b57
Jim Browning <jamalebrowning@gmail.com>
2020-08-22 13:54:19 -0500

    delete player

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import playerShape from '../../helpers/props/playerShape';
 
@@ -7,7 +8,14 @@ import './Player.scss';
 class Player extends React.Component {
   static propTypes = {
     player: playerShape.playerShape,
+    deletePlayer: PropTypes.func.isRequired,
   }
+
+  deletePlayerEvent = (e) => {
+    e.preventDefault();
+    const { player, deletePlayer } = this.props;
+    deletePlayer(player.id);
+  };
 
   render() {
     const { player } = this.props;
@@ -18,7 +26,7 @@ class Player extends React.Component {
       <div className="card-body">
     <h5 className="card-title">{player.name}</h5>
     <p className="card-text">{player.position}</p>
-    <button className="btn btn-danger">X</button>
+    <button className="btn btn-danger" onClick={this.deletePlayerEvent}>X</button>
       </div>
       </div>
     );
